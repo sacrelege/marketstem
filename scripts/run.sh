c326ebe8118aac66b5eaf0659d4b2e3ddcd3cc12
jamespedwards42 <james.p.edwards42@gmail.com>
2015-05-26 12:05:03 -0700

    Start to wire up continuous deployment.

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,15 +12,16 @@ SERVER_OPS="-server -XX:MaxMetaspaceSize=256m"
 WEB_SERVER_OPS="-javaagent:$INSTALL_DIR/newrelic/newrelic.jar -Dnewrelic.enable.java.8"
 GC_OPS="-XX:+UseConcMarkSweepGC -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSParallelRemarkEnabled -XX:+ParallelRefProcEnabled -XX:CMSInitiatingOccupancyFraction=80 -XX:+UseCMSInitiatingOccupancyOnly"
 
-if [[ -z $(ps ax | egrep "$PROJECT_NAME.*jar|gradle" | grep 'grep' -v) ]]
+if [[ -z $(ps ax | egrep "$PROJECT_NAME.*jar|shadowJar" | grep 'grep' -v) ]]
     then
     rm -f "$INSTALL_DIR/nohup.out"
     systemMemory=$( awk '/MemTotal/{print $2}' /proc/meminfo )
 
-    if [ "$systemMemory" -gt 2000000 ]; then
-      nohup java -Xmx1600M $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE AGGREGATE_TICKER MARKETSTEM_HTTP >>console.out 2>&1 &
-    elif [ "$systemMemory" -gt 1000000 ]; then
-      nohup java -Xmx700M $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE MARKETSTEM_HTTP >>console.out 2>&1 &
+    if [ "$systemMemory" -gt 2000000 ]
+      then
+         nohup java $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE AGGREGATE_TICKER MARKETSTEM_HTTP >>console.out 2>&1 &
+      else
+         nohup java $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE MARKETSTEM_HTTP >>console.out 2>&1 &
     fi
 fi
 

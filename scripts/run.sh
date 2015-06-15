8a1a046c490f37d13f308225be88222bf681c679
jamespedwards42 <james.p.edwards42@gmail.com>
2015-06-15 08:14:49 -0700

    add caching vm param.

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,7 @@ JAR_OP="-jar $REPO_DIR/build/libs/$PROJECT_NAME*.jar"
 SERVER_OPS="-server -XX:MaxMetaspaceSize=256m"
 WEB_SERVER_OPS="-javaagent:$REPO_DIR/newrelic/newrelic.jar -Dnewrelic.enable.java.8"
 GC_OPS="-XX:+UseConcMarkSweepGC -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSParallelRemarkEnabled -XX:+ParallelRefProcEnabled -XX:CMSInitiatingOccupancyFraction=80 -XX:+UseCMSInitiatingOccupancyOnly"
+NETWORK_OPS="-Dsun.net.inetaddr.ttl=30"
 
 if [[ -z $(ps ax | egrep "$PROJECT_NAME.*jar|shadowJar" | grep 'grep' -v) ]]
     then
@@ -17,9 +18,9 @@ if [[ -z $(ps ax | egrep "$PROJECT_NAME.*jar|shadowJar" | grep 'grep' -v) ]]
 
     if [ "$systemMemory" -gt 2000000 ]
       then
-         nohup java $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE AGGREGATE_TICKER MARKETSTEM_HTTP >>console.out 2>&1 &
+         nohup java $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $NETWORK_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE AGGREGATE_TICKER MARKETSTEM_HTTP >>console.out 2>&1 &
       else
-         nohup java $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE MARKETSTEM_HTTP >>console.out 2>&1 &
+         nohup java $SERVER_OPS $GC_OPS $WEB_SERVER_OPS $NETWORK_OPS $JAR_OP MARKETSTEM MARKET_DATA_CACHE MARKETSTEM_HTTP >>console.out 2>&1 &
     fi
 fi
 
